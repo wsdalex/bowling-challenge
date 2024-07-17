037d88dc0a3b59b44d845e86feff48a9907d0934
Will <williama149@gmail.com>
2024-07-17 16:08:32 +0100

    create user interface files and test

---
+++ tests/userInterface.test.js
@@ -0,0 +1,8 @@
+const UserInterface = require("../lib/userInterface");
+
+describe("UserInterface", () => {
+  let userInterface = null;
+  beforeEach(() => {
+    userInterface = new UserInterface();
+  });
+});
