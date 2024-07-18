48ec88f61f23af3d28061e8cdb8ce89ce14172e5
Will <williama149@gmail.com>
2024-07-18 10:52:30 +0100

    Simple unit tests added

--- tests/userInterface.test.js
+++ tests/userInterface.test.js
@@ -1,8 +1,37 @@
 const UserInterface = require("../lib/userInterface");
+const Scorecard = require("../lib/scorecard");
+
+const readline = require("readline");
+
+jest.mock("../lib/scorecard"); // Mock Scorecard module
 
 describe("UserInterface", () => {
-  let userInterface = null;
+  let userInterface;
+  let scorecard;
+
   beforeEach(() => {
-    userInterface = new UserInterface();
+    Scorecard.mockClear();
+    scorecard = new Scorecard();
+    userInterface = new UserInterface(scorecard);
+  });
+
+  test("Add player", () => {
+    userInterface.addPlayer(2);
+    expect(userInterface.players).toEqual([scorecard, scorecard]);
+  });
+
+  test("Get score", () => {
+    userInterface.addPlayer(2);
+    scorecard.calculateScore.mockImplementation(() => 0);
+    expect(userInterface.getPlayerScore(scorecard)).toBe(0);
+    expect(scorecard.calculateScore).toHaveBeenCalledTimes(1);
+  });
+
+  test("Add frame", () => {
+    userInterface.addPlayer(2);
+    scorecard.calculateScore.mockImplementation(() => 7);
+    userInterface.addFrame(scorecard, 2, 5);
+    expect(scorecard.addFrame).toHaveBeenCalledTimes(1);
+    expect(userInterface.getPlayerScore(scorecard)).toBe(7);
   });
 });
